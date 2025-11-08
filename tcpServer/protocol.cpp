00ac7ea074f4dcc58439d245635b8490510f4aa1
ZCL <2896435558@qq.com>
2025-11-08 11:52:46 +0800

    first

---
+++ tcpServer/protocol.cpp
@@ -0,0 +1,15 @@
+#include "protocol.h"
+#include <cstring>
+
+PDU *mkPDU(unit uiMsgLen)
+{
+    unit uiPDULen = sizeof(PDU) + uiMsgLen;
+    PDU *pdu = (PDU*)malloc(uiPDULen);
+    if(NULL == pdu){
+        exit(EXIT_FAILURE);
+    }
+    memset(pdu, 0, uiPDULen);
+    pdu->uiMsgLen = uiMsgLen;
+    pdu->uiPDULen = uiPDULen;
+    return pdu;
+}
